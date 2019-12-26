7b1bf30d929d559b71bbd37bfdd59545e702db65
vamshikrishna3777 <vamshikrishnapatelkolagani@gmail.com>
2019-12-26 13:43:20 +0530

    fixing css linking and image

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port=process.env.PORT||3000
 app.use(express.static("public"))
-app.get('/', (req, res) => res.sendFiles(__dirname+"/public/html/resume.html"))
+app.get('/', (req, res) => res.sendFile(__dirname+"/public/html/resume.html"))
+//app.get('/', (req, res) => res.sendFile(__dirname+"/public/css/style.css"))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
